b842775d29db988a60851837c541d55bf71b16ef
Daniel <herrnichols@gmail.com>
2019-10-01 14:17:09 -0400

    finished

--- src/drills.js
+++ src/drills.js
@@ -8,54 +8,54 @@ const knexInstance = knex({
 
 // 1. Get all items that contain Text.
 
-// function getItemsWithText(searchTerm) {
-//   knexInstance
-//     .select('name')
-//     .from('shopping_list')
-//     .where('name', 'ILIKE', `%${searchTerm}%`)
-//     .then(result => {
-//       console.log(result)
-//     })
-//     .finally(()=>knexInstance.destroy());
-// }
+function getItemsWithText(searchTerm) {
+  knexInstance
+    .select('name')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log(result)
+    })
+    .finally(()=>knexInstance.destroy());
+}
 
-// getItemsWithText('b')
+getItemsWithText('bacon');
 
 // 2. Get all items paginated
 
-// function getPaged(pageNumber) {
-//   const perPage = 6
-//   const offset = perPage * (pageNumber -1)
-//   knexInstance
-//     .select('name', 'price', 'category')
-//     .from('shopping_list')
-//     .limit(perPage)
-//     .offset(offset)
-//     .then(result => {
-//       console.log(result)
-//     })
-//     .finally(() => knexInstance.destroy())
-// }
+function getPaged(pageNumber) {
+  const perPage = 6
+  const offset = perPage * (pageNumber -1)
+  knexInstance
+    .select('name', 'price', 'category')
+    .from('shopping_list')
+    .limit(perPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result)
+    })
+    .finally(() => knexInstance.destroy())
+}
 
-// getPaged(1)
+getPaged(3)
 
 //3. Get all idems added after date.
 
-// function getByDate(daysAgo) {
-//   knexInstance
-//     .select('name', 'date_added')
-//     .where('date_added', '>', knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo))
-//     .from('shopping_list')
-//     .orderBy([
-//       {column: 'date_added', order: 'ASC'}
-//     ])
-//     .then(result => {
-//       console.log(result)
-//     })
-//     .finally(() => knexInstance.destroy());
-// }
+function getByDate(daysAgo) {
+  knexInstance
+    .select('name', 'date_added')
+    .where('date_added', '>', knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo))
+    .from('shopping_list')
+    .orderBy([
+      {column: 'date_added', order: 'ASC'}
+    ])
+    .then(result => {
+      console.log(result)
+    })
+    .finally(() => knexInstance.destroy());
+}
 
-// getByDate(2);
+getByDate(2);
 
 //4. Get the total costs for each category.
 
